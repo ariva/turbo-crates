a86c8d8b21060c3c36ca5f586ea67bfdbf241267
Arunas Ivanauskas <236536+ariva@users.noreply.github.com>
2023-04-18 00:05:57 -0400

    changes in testing/proc_macros ui test cases: added failed/success tests

---
+++ crates/testing/proc_macros/tests/ui/success/safe_tests_happy_path_default.rs
@@ -0,0 +1,15 @@
+use turbo_crates_testing_proc_macros::*;
+
+fn main() {}
+
+#[safe_tests]
+pub(crate) mod test {
+    const TEST_ENV_VAR: &str = "TEST_ENV_VAR";
+
+    #[safe_test]
+    fn it_works1() {
+        std::env::set_var(TEST_ENV_VAR, "test1");
+        let test_value = std::env::var(TEST_ENV_VAR).unwrap();
+        assert_eq!(test_value, "test1");
+    }
+}
